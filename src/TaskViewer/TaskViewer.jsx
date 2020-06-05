222384659f63e1eb0523261b350dcad6811bf2e8
nmikhalaki <nnegru@wiley.com>
2020-06-05 11:54:41 +0300

    added edit feature

--- src/TaskViewer/TaskViewer.jsx
+++ src/TaskViewer/TaskViewer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import './taskViewer.css'
 import {Button} from '@material-ui/core';
+import EditIcon from '@material-ui/icons/Edit';
 
 class TaskViewer extends React.Component {
     constructor(props) {
@@ -13,7 +14,6 @@ class TaskViewer extends React.Component {
         this.handleClickEdit = this.handleClickEdit.bind(this);
         this.handleClickDone = this.handleClickDone.bind(this);
     }
-
     handleClickDelete = (index, e) => {
         this.taskList.splice(index, 1)
         localStorage.setItem('taskList', JSON.stringify(this.taskList))
@@ -32,6 +32,22 @@ class TaskViewer extends React.Component {
             taskList: this.taskList
         })
     }
+
+    handleEditTitle = (index, e) => {
+        this.taskList[index].title = window.prompt('Title', this.taskList[index].title)
+        localStorage.setItem('taskList', JSON.stringify(this.taskList))
+        this.setState({
+            taskList: this.taskList
+        })
+    }
+
+    handleEditDescription = (index, e) => {
+        this.taskList[index].description = window.prompt('Description', this.taskList[index].description)
+        localStorage.setItem('taskList', JSON.stringify(this.taskList))
+        this.setState({
+            taskList: this.taskList
+        })
+    }
     // const taskList = JSON.parse(localStorage.getItem('taskList'))
     // taskList.sort((a, b) => {
     //     if (a.title > b.title)
@@ -42,25 +58,36 @@ class TaskViewer extends React.Component {
     // })
     render() {
         const tasks = []
-        debugger
+        // debugger
         this.state.taskList.forEach((task, index) => {
             tasks.push(
                 <div className='task-item' key={`task-item-${index}`}>
                     <div className='task-item-card'>
                         <div className='task-item-title'>
-                            {task.title}
+                            <div>
+                                <EditIcon fontSize='small' onClick={(e) => this.handleEditTitle(index, e)}></EditIcon>
+                            </div>
+                            <div>
+                                {task.title}
+                            </div>
                         </div>
                         <div className='task-item-description'>
-                            {task.description}
+                            <div>
+                                <EditIcon fontSize='small' onClick={(e) => this.handleEditDescription(index, e)}></EditIcon>
+                            </div>
+                            <div>
+                                {task.description}
+                            </div>
                         </div>
                         <div className='task-item-status'>
                             Completed: {task.completed.toString()}
                         </div>
                     </div>
                     <div className='task-item-option'>
-                        <Button size='small' variant='outlined' color='primary' onClick={(e) => this.handleClickDone(index, e)}>Done</Button>
-                        <Button size='small' variant='outlined' color='default' onClick={this.handleClickEdit}>Edit</Button>
-                        <Button size='small' variant='outlined' color='secondary' onClick={(e) => this.handleClickDelete(index, e)}>Delete</Button>
+                        <Button size='small' variant='outlined' color='primary'
+                                onClick={(e) => this.handleClickDone(index, e)}>Done</Button>
+                        <Button size='small' variant='outlined' color='secondary'
+                                onClick={(e) => this.handleClickDelete(index, e)}>Delete</Button>
                     </div>
                 </div>
             )
