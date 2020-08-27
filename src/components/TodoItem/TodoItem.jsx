b285d29c7217c765b4c06a5d5b1f35a454d584a1
nmikhalaki <nnegru@wiley.com>
2020-08-27 12:33:16 +0300

    refactor TodoItem

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -1,139 +1,34 @@
-import React from 'react';
-import {connect} from 'react-redux'
+import React from 'react'
+import './todoItem.css'
 import {
   Card,
   Button,
   CardActions,
   CardContent,
-  Typography,
-  TextField,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle,
+  Typography
 } from '@material-ui/core'
 import EditIcon from '@material-ui/icons/Edit'
 import DoneIcon from '@material-ui/icons/Done'
 import DeleteIcon from '@material-ui/icons/Delete'
-import {
-  editTodo,
-  deleteTodo,
-  completeTodo
-} from '../../actions'
-import './todoItem.css'
 
 
-class TodoItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...this.props.todo,
-      open: false,
-    }
-  }
+const TodoItem = (props) => (
+  <Card variant="outlined">
+    <CardContent>
+      <Typography variant="h6" component="h2">
+        {props.todo.title}
+      </Typography>
+      <Typography variant="body2" component="p">
+        {props.todo.description}
+      </Typography>
+    </CardContent>
+    <CardActions style={{justifyContent: 'center'}}>
+      <Typography variant="body2" component="p">Completed: {props.todo.completed.toString()}</Typography>
+      <Button onClick={() => props.handleClickDone(props.todo.id)} size="small"><DoneIcon/></Button>
+      <Button onClick={() => props.handleClickEdit(props.todo)} size="small"><EditIcon/></Button>
+      <Button onClick={() => props.handleClickDelete(props.todo.id)} size="small"><DeleteIcon/></Button>
+    </CardActions>
+  </Card>
+)
 
-  handleClickEdit = () => {
-    this.setState({
-      open: true
-    });
-  };
-
-  handleUpdateTitle = title => {
-    this.setState({
-      title: title
-    });
-  };
-
-  handleUpdateDescription = description => {
-    this.setState({
-      description: description
-    });
-  };
-
-  handleClickCancel = () => {
-    this.setState({
-      open: false
-    });
-  };
-
-  handleClickSave = () => {
-    this.props.editTodo({
-      id: this.state.id,
-      title: this.state.title,
-      description: this.state.description
-    })
-    this.setState({
-      open: false
-    });
-  }
-
-  handleDelete = () => {
-    this.props.deleteTodo(this.props.todo.id)
-  }
-
-  handleDone = () => {
-    this.props.completeTodo(this.props.todo.id)
-  }
-
-  render() {
-    return (
-      <Card variant="outlined">
-        <CardContent>
-          <Typography variant="h6" component="h2">
-            {this.props.todo.title}
-          </Typography>
-          <Typography variant="body2" component="p">
-            {this.props.todo.description}
-          </Typography>
-        </CardContent>
-        <CardActions style={{justifyContent: 'center'}}>
-          <Typography variant="body2" component="p">Completed: {this.props.todo.completed.toString()}</Typography>
-          <Button onClick={() => this.handleDone()} size="small"><DoneIcon></DoneIcon></Button>
-          <Button onClick={() => this.handleClickEdit()} size="small"><EditIcon></EditIcon></Button>
-          <Button onClick={() => this.handleDelete()} size="small"><DeleteIcon></DeleteIcon></Button>
-        </CardActions>
-        <Dialog open={this.state.open} onClose={this.handleClickCancel} aria-labelledby="form-dialog-title">
-          <DialogTitle id="form-dialog-title">Edit Todo</DialogTitle>
-          <DialogContent>
-            <TextField
-              defaultValue={this.props.todo.title}
-              autoFocus
-              margin="dense"
-              id="title"
-              label="Title"
-              onChange={e => this.handleUpdateTitle(e.target.value)}
-              fullWidth
-            />
-            <TextField
-              defaultValue={this.props.todo.description}
-              margin="dense"
-              id="description"
-              label="Description"
-              multiline
-              rows={4}
-              onChange={e => this.handleUpdateDescription(e.target.value)}
-              fullWidth
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={this.handleClickCancel} color="primary">
-              Cancel
-            </Button>
-            <Button onClick={this.handleClickSave} color="primary">
-              Save
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Card>
-    );
-  }
-}
-
-export default connect(
-  null,
-  {
-    editTodo,
-    deleteTodo,
-    completeTodo,
-  }
-)(TodoItem)
+export default TodoItem
